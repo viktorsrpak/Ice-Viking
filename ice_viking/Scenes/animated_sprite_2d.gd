c15719523606a54c27c6ad78404ae674aa29539d
Viktor Srpak <viktorsrpak06@gmail.com>
2025-01-19 21:38:40 +0100

    level1 dizajn i lava i fix

---
+++ ice_viking/Scenes/animated_sprite_2d.gd
@@ -0,0 +1,6 @@
+extends AnimatedSprite2D
+
+# Funkcija koja se poziva kada je objekat spreman
+func _ready():
+	# Pokušajte da pokrenete animaciju
+	play("lava")  # Zamenite "ime_animacije" sa stvarnim imenom animacije
