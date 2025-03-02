984c3ee24006db985231ab26c237ba8e6fca6068
vanjasalamon <vanjasalamon9@gmail.com>
2025-03-02 18:23:58 +0100

    Level_select scena
    - dodana level_select.tscn

--- ice_viking/Scenes/level_1.gd
+++ ice_viking/Scenes/level_1.gd
@@ -3,4 +3,4 @@ extends Node2D
 
 
 func _on_menu_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://Scenes/Main_menu.tscn")
+	get_tree().change_scene_to_file("res://Scenes/level_select.tscn")
--- ice_viking/Scenes/main_menu.gd
+++ ice_viking/Scenes/main_menu.gd
@@ -3,7 +3,7 @@ extends TextureRect
 
 
 func _on_play_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://Scenes/Level_1.tscn")
+	get_tree().change_scene_to_file("res://Scenes/level_select.tscn")
 
 
 
