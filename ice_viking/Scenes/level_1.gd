26b085049f1794d47d6abd1aa70b4ca99c7f4a25
vanjasalamon <vanjasalamon9@gmail.com>
2025-04-19 17:04:17 +0200

    Options nadodano i portal

--- ice_viking/Scenes/level_1.gd
+++ ice_viking/Scenes/level_1.gd
@@ -10,3 +10,13 @@ func _ready():
 
 func _on_menu_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://Scenes/level_select.tscn")
+
+
+func _on_lever_body_entered(_Node2D) -> void:
+	$Lever/AnimatedSprite2D.frame = 1
+	$Portal/AnimationPlayer.play("Slide")
+
+
+
+func _on_area_2d_body_entered(_body: Node2D) -> void:
+	get_tree().change_scene_to_file("res://Scenes/Main_menu.tscn")
