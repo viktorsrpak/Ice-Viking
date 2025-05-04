865e22c291ddd001e9b2a625254729edc923b1e0
vanjasalamon <vanjasalamon9@gmail.com>
2025-05-04 20:57:05 +0200

    Za završni sam nkj promijenil

--- ice_viking/lava.gd
+++ ice_viking/lava.gd
@@ -5,10 +5,8 @@ func _ready():
 		$Area2D.body_entered.connect(_on_area_2d_body_entered)
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
-	print("Detektovan ulazak:", body.name)  # Debug ispis
-	if body.is_in_group("Player"):  # Proverava da li je objekat igrač
-		print("Igrač je dotakao lavu!")
-		call_deferred("_change_to_game_over")  # Odlaže promenu scene
+	if body.is_in_group("Player"):  
+		call_deferred("_change_to_game_over") 
 
 func _change_to_game_over():
-	get_tree().change_scene_to_file("res://Scenes/game_over.tscn")  # Prebacuje na game_over.tscn
+	get_tree().change_scene_to_file("res://Scenes/game_over.tscn")  
