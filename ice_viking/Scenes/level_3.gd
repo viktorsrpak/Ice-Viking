e200473bfcc2f47d9f06e109de117dc30c59b814
Viktor Srpak <viktorsrpak06@gmail.com>
2025-05-15 20:04:47 +0200

    level2

--- ice_viking/Scenes/level_3.gd
+++ ice_viking/Scenes/level_3.gd
@@ -6,7 +6,17 @@ func _ready():
 	if not player:
 		print("Greška: Igrač nije pronađen!")
 	else:
-		print("Igrač pronađen:", player.name)
+		print("Igrač se stvorio: ", player.name)
 
 func _on_menu_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://Scenes/level_select.tscn")
+
+
+func _on_lever_body_entered(_Node2D) -> void:
+	$Lever/AnimatedSprite2D.frame = 1
+	$Portal/AnimationPlayer.play("Slide")
+
+
+
+func _on_area_2d_body_entered(_body: Node2D) -> void:
+	get_tree().call_deferred("change_scene_to_file", "res://Scenes/main_menu.tscn")
--- ice_viking/Scenes/score_hud.gd
+++ ice_viking/Scenes/score_hud.gd
@@ -8,12 +8,9 @@ func _update_score_label():
 	$Label.text = "SCORE: " + str(Global.score)
 
 
-
 func _on_coins_1_coin_colected() -> void:
 	_update_score_label()
- 
 
 
-
-func _on_coins_coin_colected() -> void:
+func _on_coins_2_coin_colected() -> void:
 	_update_score_label()
